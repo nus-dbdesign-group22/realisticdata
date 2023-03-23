ccf05efeaa9f6d8df0224e4f3f9c43dcccc83ed4
OscarJohansson1 <osjoh99@gmail.com>
2023-03-23 23:41:03 +0800

    Added basic code in order to run the tool in terminal

--- generate_data.py
+++ generate_data.py
@@ -2,6 +2,10 @@
 import os
 import sys
 import getopt
+import time
+
+import pandas as pd
+import re
 
 """
 Run the tool from the terminal using the command
@@ -31,22 +35,99 @@ def read_input(input_directory, input_file):
     os.chdir(input_directory)
 
     with open(input_file, 'r') as file:
-        for line in file.readlines():
+        line = file.readline()
+        tables = {}
+        while line:
             # Remove indentation, whitespaces, etc.
             line = line.strip()
             # Ignore comments
-            if line.startswith("#"):
-                continue
             if len(line) == 0:
-                continue
-            else:
-                print(str(line))
+                pass
+            elif line.startswith("#"):
+                pass
+            elif line.startswith("CREATE TABLE"):
+                create_table(file, line, tables)
+            line = file.readline()
 
     with open('results.txt', 'w') as results:
         results.write("We are currently working on this part...\n")
         results.write("\n")
         results.write("Welcome back later :)")
 
+
+def create_table(file, line, tables):
+    """
+    Iterates through the text file until the end of the CREATE TABLE statement is found, while building the table.
+    :param file:    The current text file
+    :param line:    The current line
+    :param tables:  A dictionary with all the tables, with the table name as key
+    :return:        Updates the tables dictionary
+    """
+    statement = line.split(" ")
+    df = pd.DataFrame(index = ['0'])
+    while True:
+        line = file.readline()
+        statement.extend(re.split("\W+", str(line)))
+        if ");" in str(line):
+            break
+    for i in reversed(range(len(statement))):
+        if statement[i] in "(),;. ":
+            statement.pop(i)
+    i = 0
+    foreign_keys = []
+    while i < len(statement):
+        if statement[i] == "CREATE":
+            try:
+                if statement[i + 1] == "TABLE":
+                    print("Creating table", statement[i + 2], "...")
+                    tables[statement[i + 2]] = df
+                    i += 3
+                    while statement[i] != "PRIMARY" and statement[i] != "FOREIGN":
+                        df[statement[i]] = statement[i + 1]
+                        i += 2
+                else:
+                    raise Exception("CREATE TABLE formatted incorrectly")
+            except IndexError:
+                raise Exception("CREATE TABLE formatted incorrectly")
+        elif statement[i] == "PRIMARY":
+            try:
+                if statement[i + 1] == "KEY":
+                    offset = 2
+                    keys = []
+                    while i + offset < len(statement):
+                        if statement[i + offset] == "FOREIGN":
+                            break
+                        keys.append(statement[i + offset])
+                        offset += 1
+                    df["PRIMARY_KEY"] = [keys]
+                    i += offset
+                else:
+                    raise Exception("CREATE TABLE formatted incorrectly")
+            except IndexError:
+                raise Exception("CREATE TABLE formatted incorrectly")
+        elif statement[i] == "FOREIGN":
+            try:
+                if statement[i + 1] == "KEY":
+                    offset = 2
+                    ref_offset = offset + 1
+                    while statement[i + ref_offset] != "REFERENCES":
+                        ref_offset += 1
+                    ref_offset += 1
+                    n_keys = ref_offset - offset - 1
+                    for k in range(n_keys):
+                        # Append [attribute_name, foreign table, foreign attribute_name]
+                        foreign_keys.append([statement[i + offset + k],
+                                            statement[i + ref_offset],
+                                            statement[i + ref_offset + k + 1]])
+                    df['FOREIGN_KEYS'] = [foreign_keys]
+                    i += ref_offset + n_keys
+                else:
+                    raise Exception("CREATE TABLE formatted incorrectly")
+            except IndexError:
+                raise Exception("CREATE TABLE formatted incorrectly")
+        else:
+            i += 1
+    print(df)
 
 def set_up():
     input_directory = input_file = None
