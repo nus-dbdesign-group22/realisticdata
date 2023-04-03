19fac2e18819a6fcb104dd13a15cfaa0199bfe76
Duy Nguyen <4320964+duyng404@users.noreply.github.com>
2023-04-03 22:16:18 +0800

    minor changes

--- app/main.py
+++ app/main.py
@@ -6,12 +6,12 @@ from generator import Generator
 def main():
     # Defining and parsing the command line flags
     parser = argparse.ArgumentParser(
-        description='generate realistic mock data based on complex user inputs',
+        description='Generate realistic mock data based on complex user inputs.',
     )
 
-    parser.add_argument('-i', '--input', help='path to the input file', required=True)
-    parser.add_argument('-o', '--output', help='(optional) path to the output file. If not specified will print to stdout')
-    parser.add_argument('-f', '--format', help='(optional) output format -- options are: csv, sql. Default is csv (feature is WIP)')
+    parser.add_argument('-i', '--input', help='Path to the input file', required=True)
+    parser.add_argument('-o', '--output', help='(optional) Path to the output file. If not specified will print to stdout')
+    parser.add_argument('-f', '--format', help='(optional) Output format -- options are: csv, sql. Default is csv (feature is WIP)')
     args = parser.parse_args()
 
     input_reader = InputReader(args.input, args.output, args.format)
