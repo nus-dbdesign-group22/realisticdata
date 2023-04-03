7cdc6bd8522c35e8097ac8561b0fd740989d7c54
khurushbengali <khurushbengali28@gmail.com>
2023-04-03 14:37:16 +0800

    Updated string type. User has to mention values to randomize in case of string types

--- app/generator_types/stringtype.py
+++ app/generator_types/stringtype.py
@@ -1,13 +1,21 @@
+import random
+
 from generator_types.base import BaseTypeGenerator
+
 
 class String(BaseTypeGenerator):
     def get_next_value(self, related_values=None) -> any:
-        options = self.column.options
+        options = self.options
+        sample = ""
         # if has ref dependence, choose from list of ref values
         # if has FD dependence, check if has already generated, else 
-            # need to know current index
+        # need to know current index
         # if have none of those, generate normally
-        if "unique" in options.keys() and options["unique"]=="true":
+        if "unique" in options.keys() and options["unique"] == "true":
             print("yes unique")
         # if unique then remember it
-        return "this is a random string"
+        if "values" in options.keys():
+            values = list(options["values"].split(","))
+            print(values)
+            sample = random.choice(values)
+        return sample
